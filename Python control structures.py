6b2d79ea608ac0234943235d15e7498f99cab049
Malon kipleting <malonkipleting790@gmail.com>
2022-11-10 19:13:01 +0300

    First commit

---
+++ Python control structures.py
@@ -0,0 +1,10 @@
+#if statement format
+#if expression
+#     statements
+  #Expressions
+  #use relational operators (<,>,=,>=,<=, is, ==, !=)
+
+num1 = 12
+num2 = 10
+if num1 > num2:
+    print("num1 is greater than num2")
